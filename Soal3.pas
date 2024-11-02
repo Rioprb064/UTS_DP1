d0311fa33f10d532a50d3e5b6c2f2d9a0fead82c
Rioprb064 <rionaldopurba2006@gmail.com>
2024-11-02 15:54:39 +0700

    Update Soal3.pas

--- Soal3.pas
+++ Soal3.pas
@@ -1,1 +1,24 @@
-//ketik kode jawaban noo 3 kamu disini
+uses crt;
+var //pendeklarasian variabel
+    n,x,y : integer; //mendeklaraasikan nilai variabel n,x,y bertipe data integer
+
+begin //memulai program
+    clrscr;
+    write ('n : '); //input data
+    readln (n); //membaca nilai inputan sebagai n
+    
+    if n mod 2 = 1 then //operasi kondisi jika nilai n bernilai ganjil
+    begin //memulai operasi kondisi
+        for x:=1 to n do //menginisialisasikan nilai x dimulai dari 1 hingga n
+        begin //memulai operasi untuk x
+            for y:=1 to n do //menginisialisasikan nilai y dimulai dari 1 hingga n
+            begin //memulai operasi untuk y
+                if  (x=y) or (x+y=n+1) then //operasi kondisi untuk output
+                    write('*') //output jika kondisi terpenuhi
+                else //operasi kondisi untuk output
+                    write(' ');//output jika kondisi tidak terpenuhi
+            end; //mengakhiri operasi untuk y
+            writeln;
+        end; //mengakhiri operasi untuk x
+    end; //mengakhiri operasi kondisi
+end. //mengakhiri program
